02e3fe75e195983031f138d044ad3e5bc32a4cb3
abdallah-azzouni <xara25az@gmail.com>
2026-01-18 17:34:21 +0300

    feat: implement createNote functionality and add CreateNote component for note creation

--- src/lib/stores/userData.ts
+++ src/lib/stores/userData.ts
@@ -1,14 +1,21 @@
 
-import { persisted } from 'svelte-persisted-store';
 import { browser } from '$app/environment';
+import { persisted } from 'svelte-persisted-store';
 
-export let isLoaded = false;
+
+export const isLoaded = persisted('isLoaded', false);
 
 if (browser) {
-    isLoaded = true;
+  if (document.readyState === 'complete') {
+    isLoaded.set(true);
+  } else {
+    window.addEventListener('load', () => isLoaded.set(true));
+  }
 }
+
 
 export const userNotes = persisted('userNotes', {
   activeProjectId: '',
-  projects : <any>[]
+  projects : <{ id: string; name: string; color: string; 
+    columns: { todo: { title: string; color: string }[]; doing: { title: string; color: string }[]; jar: { title: string; color: string }[] } }[]>[]
 });

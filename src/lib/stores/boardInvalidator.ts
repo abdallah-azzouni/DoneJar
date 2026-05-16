d564824002e1c7f6112b90cea832087011b5c5a9
abdallah-azzouni <xara25az@gmail.com>
2026-05-16 20:52:18 +0300

    refactor: remove unused boardInvalidator store

--- src/lib/stores/boardInvalidator.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const boardInvalidator = writable(0);
-
-export function invalidateBoard() {
-	boardInvalidator.update((n) => n + 1); // increment instead of bool, safer
-}

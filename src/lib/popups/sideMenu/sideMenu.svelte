ca199f64721da875836e5b32058e6a2a13cb9db8
abdallah-azzouni <a.azzouni25@gmail.com>
2026-07-15 18:27:03 +0300

    feat(sidemenu): make side menu a bottom sheet on mobile
    Use ThemedDialog's new dragToClose behavior on mobile viewports
    (via MediaQuery) while keeping the existing right-anchored panel
    on desktop. Add drag handle indicator and adjust layout classes
    for full-width/bottom positioning on small screens.

--- src/lib/popups/sideMenu/sideMenu.svelte
+++ src/lib/popups/sideMenu/sideMenu.svelte
@@ -5,50 +5,58 @@
 	import { resolve } from '$app/paths';
 	import { getAppState } from '$lib/stores/appState.svelte';
 	import { sessionStore } from '$lib/stores/currentUser.svelte';
+	import { MediaQuery } from 'svelte/reactivity';
+
+	const isDesktop = new MediaQuery('(min-width: 1024px)');
 </script>
 
 <ThemedDialog
-	mt="mt-10"
-	w="w-1/3"
-	h="h-3/4"
-	cls="ml-auto mr-12 "
+	mt="sm:mt-10 mt-auto"
+	w="sm:w-1/3 w-full"
+	h="sm:h-3/4 max-h-2/3"
+	cls="sm:ml-auto sm:mr-12 mx-auto rounded-b-none sm:rounded-2xl max-w-none"
+	label="Side Menu"
 	isOpen={sideMenuStore.isOpen}
 	onClose={() => sideMenuStore.close()}
 	closeOnBackdrop={true}
+	dragToClose={!isDesktop.current}
 >
-	{#each sideMenuItems as item (item.index)}
-		{#if item.label !== 'profile'}
-			<button
-				class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
-				onclick={() => item.action()}
-			>
-				<span>{item.label}</span>
-			</button>
-		{:else if getAppState() == 'LOGGED_IN'}
-			<hr class="my-4 mt-auto border-2 border-dashed border-gray-300" />
-			<button
-				class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
-				onclick={() => item.action()}
-			>
-				<img
-					//src={getURLfromObject($currentUser, $currentUser?.avatar)}
-					alt="Avatar"
-					class=" mr-2 inline-block size-9 rounded-full border-2 border-black p-1"
-				/>
-				<span>{sessionStore.current?.user?.user_metadata?.display_name}</span>
-			</button>
-		{:else}
-			<hr class="my-4 mt-auto border-2 border-dashed border-gray-300" />
+	<div class="flex h-full flex-col [*&>button]:bg-white">
+		<hr class="mx-auto mb-6 w-1/3 rounded-3xl border-2 border-gray-500 sm:hidden" />
+		{#each sideMenuItems as item (item.index)}
+			{#if item.label !== 'profile'}
+				<button
+					class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
+					onclick={() => item.action()}
+				>
+					<span>{item.label}</span>
+				</button>
+			{:else if getAppState() == 'LOGGED_IN'}
+				<hr class="my-4 mt-auto border-2 border-dashed border-gray-300" />
+				<button
+					class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
+					onclick={() => item.action()}
+				>
+					<img
+						//src={getURLfromObject($currentUser, $currentUser?.avatar)}
+						alt="Avatar"
+						class=" mr-2 inline-block size-9 rounded-full border-2 border-black p-1"
+					/>
+					<span>{sessionStore.current?.user?.user_metadata?.display_name}</span>
+				</button>
+			{:else}
+				<hr class="my-4 mt-auto border-2 border-dashed border-gray-300" />
 
-			<button
-				class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
-				onclick={() => {
-					sideMenuStore.close();
-					goto(resolve('/auth/login'));
-				}}
-			>
-				<span>Login</span>
-			</button>
-		{/if}
-	{/each}
+				<button
+					class="doodle-border mb-4 w-full rounded-lg p-2 text-left font-patrick-hand text-xl"
+					onclick={() => {
+						sideMenuStore.close();
+						goto(resolve('/auth/login'));
+					}}
+				>
+					<span>Login</span>
+				</button>
+			{/if}
+		{/each}
+	</div>
 </ThemedDialog>

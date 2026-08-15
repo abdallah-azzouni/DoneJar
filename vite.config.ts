581fb35abe8ff9ef4ba8ba580c892ab675380de8
abdallah-azzouni <a.azzouni25@gmail.com>
2026-08-15 22:53:42 +0300

    feat(sentry): add @sentry/sveltekit integration and hooks

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			org: 'abdallah-azzouni-5f',
+			project: 'donejar'
+		}),
+		tailwindcss(),
+		sveltekit()
+	],
 	ssr: {
 		noExternal: ['svelte-sonner']
 	},
